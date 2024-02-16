3c72c2ca0f376cb48f5cc83b44b5b2e1161d5e35
Frank Gottinger <gottingerfrank@zoho.com>
2024-02-16 23:17:08 +0100

    chapter6: shodan, dnspython. Add modified scripts in work folder

--- chapter6/dnspython/dns_python_records.py
+++ chapter6/dnspython/dns_python_records.py
@@ -4,17 +4,17 @@ def main(domain):
     records = ['A','AAAA','NS','SOA','MX','TXT']
     for record in records:
         try:
-            responses = dns.resolver.query(domain, record)
-            print("\nRecord response ",record)
+            responses = dns.resolver.resolve(domain, record) # dns.resolver.query() method DEPRECATED !
+            print("\nRecord response ", record)
             print("-----------------------------------")
             for response in responses:
                 print(response)
         except Exception as exception:
-            print("Cannot resolve query for record",record)
+            print("Cannot resolve query for record", record)
             print("Error for obtaining record information:", exception)
 
 if __name__ == '__main__':
-	try:
-		main('google.com')
-	except KeyboardInterrupt:
-		exit()
+    try:
+        main('google.com')
+    except KeyboardInterrupt:
+        exit()
